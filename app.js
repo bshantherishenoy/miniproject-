886a094f0c8f01c1bd25146b22acdf74c7a66c5f
Aniket <anirajkul01@gmail.com>
2020-12-02 17:16:28 +0530

    authentication and db connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path")
 const session = require("express-session")
 const flash = require("connect-flash")
 const connection = require("./connection/connection") //connection
+let forcedrouting = false;
 
 //listen to port
 const PORT = process.env.PORT || 3000;
@@ -38,34 +39,113 @@ app.use(flash());
 
 //all get routes
 app.get("/", (req, res) => {
-    res.render("index")
 
+    if (req.session.loggedin === true) {
+     res.redirect("/home")
+    }
+    else {
+        res.render("index",{navbar : ''})
+    }
 })
 
 app.get("/signup", (req, res) => {
     res.render("signup", { errormessages: '' })
 })
 app.get("/signin", (req, res) => {
-    res.render("signin", { errormessages: '' ,successmessages: ''})
+    if(forcedrouting == false){
+        res.render("signin", { errormessages: '', successmessages: '' })
+    }
+    else{
+        forcedrouting = false;
+        req.flash('message', '*PLEASE LOGIN TO CONTINUE BOOKING*')
+        res.render("signin", { errormessages: req.flash('message'),successmessages: '' })
+    }
 })
 app.get("/aboutus", (req, res) => {
     res.render("aboutus")
 })
+app.get("/home",(req,res)=>{
+    if (req.session.loggedin === true) {
+        res.render("index",{navbar : 'loginsuccesful'})
+    }
+    else {
+        forcedrouting = true;
+        res.redirect("/signin")
+    }
+
+})
+app.get("/mybookings",(req,res)=>{
+    if (req.session.loggedin === true) {
+        res.send("mybookings : page to be created!")
+    }
+    else {
+        forcedrouting = true;
+        res.redirect("/signin");
+    }
+})
+app.get("/myprofile",(req,res)=>{
+    if (req.session.loggedin === true) {
+      res.render("myprofile")
+    }
+    else {
+        forcedrouting = true;
+        res.redirect("/signin");
+    }
+})
+
+app.get("/logout",(req,res)=>{
+    if (req.session.loggedin === true) {
+        req.session.loggedin = false;
+        res.redirect("/")
+      }
+      else {
+        forcedrouting = true;
+        res.redirect("/signin");
+      }
+})
+
 
 //all post routes
 app.post("/", (req, res) => {
     let data = req.body;
     // res.send(data);
+    console.log(req.session.loggedin);
     if (req.session.loggedin === true) {
         res.send(data)
     }
     else {
         req.flash('message', '*PLEASE LOGIN TO CONTINUE BOOKING*')
-        res.render("signin", { errormessages: req.flash('message') })
+        res.render("signin", { errormessages: req.flash('message'),successmessages: '' })
     }
 })
 app.post("/signin", (req, res) => {
     let data = req.body;
+    let email = req.body.email;
+    let inputpassword = req.body.password;
+
+    User.findOne({ where: { emailId: email } })
+        .then((user) => {
+            if (!user) {
+                req.flash('message', '*NO EMAIL REGISTERED FOUND!*')
+                res.render("signin", { errormessages: req.flash('message'),successmessages: '' })
+            }
+            else {
+                let registereduser = user.dataValues;
+                let userid = registereduser.userid;
+                let password = registereduser.password;
+                let email = registereduser.emailId;
+
+                if (inputpassword !== password) {
+                    req.flash('message', '*INCORRECT PASSWORD*')
+                    res.render("signin", { errormessages: req.flash('message'),successmessages: '' })
+                }
+                else{
+                    req.session.loggedin = true
+                    res.redirect("/home")
+
+                }
+            }
+        })
 })
 app.post("/signup", (req, res) => {
     let data = req.body;
@@ -86,22 +166,34 @@ app.post("/signup", (req, res) => {
         req.flash('message', 'CONFIRM PASSWORD AND PASSWORD FIELD DO NOT MATCH')
         res.render("signup", { errormessages: req.flash('message') })
     }
-    else {
-        //everything is valid
-        User.create({
-            emailId: `${email}`,
-            password: `${passwordfield}`
+    User.findOne({ where: { emailId: email } })
+        .then((user) => {
+            if (!user) {
+                console.log("no user like that found");
+                User.create({
+                    emailId: `${email}`,
+                    password: `${passwordfield}`
+                })
+                    .then(() => {
+                        console.log("new user created");
+                    })
+
+                req.flash('message', 'REGISTERED SUCCESFULLY. LOGIN NOW')
+                res.render("signin", { errormessages: '', successmessages: req.flash('message') })
+            }
+            else {
+
+                req.flash('message', 'EMAIL ID IS ALREADY REGISTERED')
+                res.render("signup", { errormessages: req.flash('message') })
+            }
         })
-            .then(() => {
-                console.log("new user created");
-            })
-        req.flash('message', 'REGISTERED SUCCESFULLY. LOGIN NOW')
-        res.render("signin", { errormessages: '',successmessages: req.flash('message') })
-    }
-
-
-
 })
 
 
+//404 error handling
+app.get("*",(req,res)=>{
+    res.status(404);
+res.send("404 Page not found")
+//page to be designed at last
+})
 
--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,8 @@
 const sequelize = require("sequelize");
 
+//easygo -- name of db 
+//root -- username of mysql
+//Roaringlyken@23 -- password of mysql
 const connection = new sequelize('easygo','root','Roaringlyken@23',{
     dialect:'mysql'
 })
